ad11c7881df8ac637382d0d4e43d00036ad5e7d0
walker-null-byte <dilnawazhossain@gmail.com>
2022-08-26 22:59:05 +0530

    2nd

--- dilluFlix/settings.py
+++ dilluFlix/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-thtd(sy0go$sn=^$_o5bd%dzj6#&*0ch^4^pj3(v(eyipv%!q('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['dilluflix.azurewebsites.net','127.0.0.1']
+ALLOWED_HOSTS = ['https://dillu-flix.herokuapp.com/','127.0.0.1']
 
 
 # Application definition
